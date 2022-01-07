3c299cad4ab53f72dd769f447607e3e3309e710d
xyntechx <xyntechx@gmail.com>
2022-01-07 21:07:26 +0800

    🚧 Test

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,33 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import { useState, useEffect } from "react";
 
 const Home: NextPage = () => {
+    const [device, setDevice] = useState(0);
+
+    const deviceType = () => {
+        // 0 = tablet, mobile
+        // 1 = desktop
+
+        const ua = navigator.userAgent;
+
+        if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+            return 0;
+        } else if (
+            /Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+                ua
+            )
+        ) {
+            return 0;
+        }
+        return 1;
+    };
+
+    useEffect(() => {
+        setDevice(deviceType());
+    });
+
     return (
         <div className={styles.container}>
             <Head>
@@ -19,7 +44,9 @@ const Home: NextPage = () => {
             <Image src="/bg.png" alt="Earth Background" layout="fill" />
 
             <main className={styles.main}>
-                <h1 className={styles.title}>Ready to Play Some Games?</h1>
+                <h1 className={device ? styles.title : styles.hidden}>
+                    Ready to Play Some Games?
+                </h1>
                 <br />
                 <div className={styles.tooltip}>
                     <Link href="/play">
